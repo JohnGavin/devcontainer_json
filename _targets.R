9baba1c918aa6b79bd5c2caa3ec7d5f755d3dab2
jg <JohnGavin@users.noreply.github.com>
2023-07-07 19:32:42 +0000

    xxx ignored

--- _targets.R
+++ _targets.R
@@ -1,11 +1,13 @@
 library(targets)
+library(cmdstanr)
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+cmdstan_version()
 library(brms)
 library(stantargets)
-path <- tempfile(pattern = "", fileext = ".stan")
-tar_stan_example_file(path = path)
-list(tar_target(zinb, read.csv("https://paul-buerkner.github.io/data/fish.csv")), 
-    fit_cmdstanr = {
-        zinbb <- tar_read(zinb)
-        brm(count ~ persons + child + camper, data = zinbb, family = zero_inflated_poisson(), 
-            backend = "cmdstanr")
-    })
+aa <- tar_target(group, rep(c("treat", "placebo"), each = 30))
+bb <- tar_target(symptom_post, c(rnorm(30, mean = 1, sd = 2), 
+    rnorm(30, mean = 0, sd = 1)))
+cc <- tar_target(dat1, data.frame(group, symptom_post))
+dd <- tar_target(fit1, brm(bf(symptom_post ~ group, sigma ~ group), 
+    data = dat1, family = gaussian(), backend = "cmdstanr"))
+list(aa, bb, cc, dd)
