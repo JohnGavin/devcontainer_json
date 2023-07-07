75ea602545e58f3baea39ad47b485bf63d5bb91e
jg <JohnGavin@users.noreply.github.com>
2023-07-07 17:26:26 +0000

    stanheaders run

--- _targets.R
+++ _targets.R
@@ -1,5 +1,11 @@
 library(targets)
+library(brms)
 library(stantargets)
 path <- tempfile(pattern = "", fileext = ".stan")
 tar_stan_example_file(path = path)
-list(ans = example(stan_model, package = "rstan", run.dontrun = TRUE))
+list(tar_target(zinb, read.csv("https://paul-buerkner.github.io/data/fish.csv")), 
+    fit_cmdstanr = {
+        zinbb <- tar_read(zinb)
+        brm(count ~ persons + child + camper, data = zinbb, family = zero_inflated_poisson(), 
+            backend = "cmdstanr")
+    })
